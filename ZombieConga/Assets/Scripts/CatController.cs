d04eb53558d10d901480bde1d520db23b7e2ed6c
Johnny Xu <gaga1227@gmail.com>
2014-10-26 16:09:22 +1100

    zombie conga - completed anim clips and transitions

---
+++ ZombieConga/Assets/Scripts/CatController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// main class
+/// </summary>
+public class CatController : MonoBehaviour {
+
+	// anim clip event handler
+	// removes gameobject from scene
+	void GrantCatTheSweetReleaseOfDeath() {
+		DestroyObject( gameObject );
+	}
+
+}
