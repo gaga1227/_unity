22bada53a3e02973dc42d3c8ecab2062327db700
Johnny Xu <gaga1227@gmail.com>
2014-10-28 23:47:46 +1100

    zombie conga - part 5 completed camera moving and background tile repeat

---
+++ ZombieConga/Assets/Scripts/BackgroundRepeater.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRepeater : MonoBehaviour {
+
+	#region vars
+	//camera transform ref
+	private Transform cameraTransform;
+	//cache bg sprite image width
+	private float spriteWidth;
+	#endregion
+
+	#region on start
+	void Start () {
+		//store camera transform ref to var
+		cameraTransform = Camera.main.transform;
+		//get ref to sprite renderer comp
+		SpriteRenderer spriteRenderer = renderer as SpriteRenderer;
+		//store sprite with to var
+		spriteWidth = spriteRenderer.sprite.bounds.size.x;
+	}
+	#endregion
+
+	#region on frame
+	void Update () {
+		//this works only when tile size is bigger than camera view size
+		//you need two bg tile to make it repeat seamlessly
+		//when second tile is fully in view and first tile is fully out of view
+		//move first tile ahead to be the new second tile
+		if((transform.position.x + spriteWidth) < cameraTransform.position.x) {
+			//get current bg pos to var
+			Vector3 newPos = transform.position;
+			//calculate new pos on x axis
+			//move out-of-view bg tile ahead of visible tile
+			newPos.x += 2.0f * spriteWidth;
+			//update back to bg pos
+			transform.position = newPos;
+		}
+	}
+	#endregion
+}
---
+++ ZombieConga/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	#region vars
+	//camera moving
+	public float speed = 1f;
+	private Vector3 newPosition;
+	#endregion
+
+	#region on start
+	void Start () {
+		//copy camera current position to new posiiton
+		newPosition = transform.position;
+	}
+	#endregion
+	
+	#region on frame
+	void Update () {
+		//moving the camera view incrementally
+		//using deltatime to maintain constant moving synced with time
+		newPosition.x = transform.position.x + Time.deltaTime * speed;
+		transform.position = newPosition;
+	}
+	#endregion
+}
