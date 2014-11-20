b9dbf74ffb5b1b9ad4af310c31c60abe09d4ae5c
Johnny Xu <gaga1227@gmail.com>
2014-11-20 22:54:53 +1100

    rocketmouse - part1 - WIP at adding bg

---
+++ RocketMouse/Assets/Scripts/MouseController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	#region private vars
+	#endregion
+
+	#region public vars
+	//jetpack force
+	public float jetpackForce = 75.0f;
+	#endregion
+
+	#region onStart
+	void Start () {
+		
+	}
+	#endregion
+
+	#region onUpdate
+	void Update () {
+		
+	}
+	#endregion
+
+	#region onFixedUpdate
+	void FixedUpdate () {
+		//enable jetpack on click
+		bool jetpackActive = Input.GetButton("Fire1");
+		//apply jetpack force if enabled
+		if (jetpackActive) {
+			rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+	#endregion
+}
