ee070a396cb135fee7a2891554d3f953eb828692
Johnny Xu <gaga1227@gmail.com>
2014-11-29 21:46:31 +1100

    rocketmouse GUI - part1 - completed adding buttons

---
+++ RocketMouse/Assets/Scripts/UIManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	#region onStart
+	void Start () {
+
+	}
+	#endregion
+	
+	#region onUpdate
+	void Update () {
+		
+	}
+	#endregion
+
+	#region methods
+	//load game level
+	public void StartGame() {
+		Application.LoadLevel("RocketMouse");
+	}
+	#endregion
+
+}
