adf535d858b47f01b0c074a645a55993ef051a82
Johnny Xu <gaga1227@gmail.com>
2014-11-30 21:05:43 +1100

    rocketmouse GUI - part2 - completed settings menu

--- RocketMouse/Assets/Scripts/UIManagerScript.cs
+++ RocketMouse/Assets/Scripts/UIManagerScript.cs
@@ -7,6 +7,7 @@ public class UIManagerScript : MonoBehaviour {
 	//animator refs
 	public Animator startButton;
 	public Animator settingsButton;
+	public Animator dialog;
 	#endregion
 
 	#region onStart
@@ -23,14 +24,33 @@ public class UIManagerScript : MonoBehaviour {
 
 	#region methods
 	//load game level
+	//------------------------------------------------------------------------
 	public void StartGame() {
 		Application.LoadLevel("RocketMouse");
 	}
 
-	//start settings intro transition
+	//open settings menu
+	//------------------------------------------------------------------------
 	public void OpenSettings() {
+		//trigger out anims for main menu buttons
 		startButton.SetBool("isHidden", true);
 		settingsButton.SetBool("isHidden", true);
+
+		//enable dialog animator and trigger in anim
+		//for settings panel
+		dialog.enabled = true;
+		dialog.SetBool("isHidden", false);
+	}
+
+	//close settings menu
+	//------------------------------------------------------------------------
+	public void CloseSettings() {
+		//trigger in anims for main menu buttons
+		startButton.SetBool("isHidden", false);
+		settingsButton.SetBool("isHidden", false);
+
+		//trigger out anim for settings panel
+		dialog.SetBool("isHidden", true);
 	}
 	#endregion
 
