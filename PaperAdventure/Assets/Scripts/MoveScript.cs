00e8194816a11b63c4d5989b1107a7d427e1f44b
Johnny Xu <gaga1227@gmail.com>
2014-12-16 22:43:46 +1100

    paper adventure - completed scene setup and starting shooting 1

---
+++ PaperAdventure/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour {
+
+	#region vars
+	// Object speed
+	public Vector2 speed = new Vector2(10, 10);
+	// Object moving direction
+	public Vector2 direction = new Vector2(-1, 0);
+	// Object movement
+	private Vector2 movement;
+	#endregion
+
+	#region onStart
+	void Start () {
+		// Movement per direction
+		movement = new Vector2(
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+	#endregion
+
+	#region onUpdate
+	void Update () {
+		
+	}
+	#endregion
+
+	#region onFixedUpdate
+	void FixedUpdate () {
+		// Move the game object
+		rigidbody2D.velocity = movement;
+	}
+	#endregion
+}
---
+++ PaperAdventure/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	#region vars
+	// player speed
+	public Vector2 speed = new Vector2(50, 50);
+	// player movement
+	private Vector2 movement;
+	#endregion
+	
+	#region onUpdate
+	void Update() {
+		// Retrieve input axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
+	#endregion
+
+	#region onFixedUpdate
+	void FixedUpdate() {
+		// Move the game object
+		rigidbody2D.velocity = movement;
+	}
+	#endregion
+}
